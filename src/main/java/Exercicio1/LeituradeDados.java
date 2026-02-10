5424a23e2e9bd30907da0b263475fc333f8a9c15
Lais Christiny Berndt <lais_berndt@estudante.sesisenai.org.br>
2026-02-10 10:03:57 -0300

    Update LeituradeDados

--- src/main/java/Exercicio1/LeituradeDados.java
+++ src/main/java/Exercicio1/LeituradeDados.java
@@ -16,7 +16,5 @@ public class LeituradeDados { //classe principal
         //aparece as informações coletadas na tela
 
         scanner.close(); //fecha o scanner
-        System.out.println("TESTE GITHUB");
-        System.out.println("TESTANDO 1 2 3...");
     }
 }
